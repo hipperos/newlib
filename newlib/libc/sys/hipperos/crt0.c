6d5e0d6e7c3ac1365fb7f74adebee8ee3732e5e7
Vladimir Svoboda <ze.vlad@gmail.com>
2018-12-13 18:50:28 +0100

    [HIPPEROS] Dummy syscalls in crt0.o (#14)
    Add a dummy implementation of all the standard C calls supported by
    Maestro in `crto.c`.
    This file will be compiled to `crt0.o` (and won't be part of `libc.a`)
    and it will be used for compiler checks (particularly useful for the
    `libstdc++` compilation).
    
    This trick is similar to the one used by *rtems*.
    
    Implementation of KERNEL-554.

--- newlib/libc/sys/hipperos/crt0.c
+++ newlib/libc/sys/hipperos/crt0.c
@@ -1,6 +1,73 @@
-/* Having a crt0 file is mandatory for some reason */
+/*
+ * Maestro Fake crt0
+ *
+ * Inspired by the RTEMS one.
+ * This allow to pass the link tests of autoconf.
+ */
 
-void _dummy()
-{
-    /* Empty function. */
-}
+#include <reent.h>
+
+#include <time.h> /* struct timespec */
+
+void maestro_provides_crt0(void) {}  /* dummy symbol so file always has one */
+
+#define MAESTRO_STUB(ret, func, body) \
+ret maestro_stub_##func body; \
+ret func body
+
+/* RTEMS provides some of its own routines including a Malloc family */
+MAESTRO_STUB(void *,malloc(size_t s), { return 0; })
+MAESTRO_STUB(void *,realloc(void* p, size_t s), { return 0; })
+MAESTRO_STUB(void, free(void* ptr), { })
+MAESTRO_STUB(_PTR, calloc(size_t s1, size_t s2), { return 0; })
+MAESTRO_STUB(int, posix_memalign(void **p, size_t si, size_t s2), { return -1; })
+
+/* stubs for functions Maestro provides */
+MAESTRO_STUB(int, clock_gettime(clockid_t clk_id, struct timespec *tp), { return -1; })
+MAESTRO_STUB(int, close (int fd), { return -1; })
+MAESTRO_STUB(int, nanosleep(const struct timespec *req, struct timespec *rem), { return -1; })
+MAESTRO_STUB(int, ftruncate(int fd, off_t length), { return -1; })
+MAESTRO_STUB(_off_t, lseek(int fd, _off_t offset, int whence), { return -1; })
+MAESTRO_STUB(int, lstat(const char *path, struct stat *buf), { return -1; })
+MAESTRO_STUB(int, open(const char *pathname, int flags, int mode), { return -1; })
+MAESTRO_STUB(_ssize_t, read(int fd, void *buf, size_t count), { return -1; })
+MAESTRO_STUB(int, stat(const char *path, struct stat *buf), { return -1; })
+MAESTRO_STUB(int, unlink(const char *pathname), { return -1; })
+MAESTRO_STUB(_ssize_t, write (int fd, const void *buf, size_t nbytes), { return -1; })
+
+/* stubs for functions from reent.h */
+MAESTRO_STUB(int, _close_r (struct _reent *r, int fd), { return -1; })
+
+MAESTRO_STUB(pid_t, getpid (), { return -1; })
+MAESTRO_STUB(pid_t, _getpid_r (struct _reent *r), { return -1; })
+#if !defined(REENTRANT_SYSCALLS_PROVIDED)
+/* cf. newlib/libc/reent/linkr.c */
+MAESTRO_STUB(int, _link_r (struct _reent *r, const char *oldpath, const char *newpath), { return -1; })
+#endif
+MAESTRO_STUB(_off_t, _lseek_r ( struct _reent *ptr, int fd, _off_t offset, int whence ), { return -1; })
+MAESTRO_STUB(int, _open_r (struct _reent *r, const char *buf, int flags, int mode), { return -1; })
+MAESTRO_STUB(_ssize_t, _read_r (struct _reent *r, int fd, void *buf, size_t nbytes), { return -1; })
+MAESTRO_STUB(int, _rename_r (struct _reent *r, const char *a, const char *b), { return -1; })
+
+MAESTRO_STUB(int, _stat_r (struct _reent *r, const char *path, struct stat *buf), { return -1; })
+MAESTRO_STUB(_CLOCK_T_, _times_r (struct _reent *r, struct tms *ptms), { return -1; })
+MAESTRO_STUB(int, _unlink_r (struct _reent *r, const char *path), { return -1; })
+MAESTRO_STUB(_ssize_t, _write_r (struct _reent *r, int fd, const void *buf, size_t nbytes), { return -1; })
+
+MAESTRO_STUB(void, _exit(int status), { while(1); })
+
+/* stdlib.h */
+MAESTRO_STUB(_PTR, _realloc_r(struct _reent *r, _PTR p, size_t s), { return 0; })
+MAESTRO_STUB(_PTR, _calloc_r(struct _reent *r, size_t s1, size_t s2), { return 0; })
+MAESTRO_STUB(_PTR, _malloc_r(struct _reent * r, size_t s), { return 0; })
+MAESTRO_STUB(_VOID, _free_r(struct _reent *r, _PTR *p), { })
+
+/* sys/mman.h */
+
+MAESTRO_STUB(void*, mmap(void* addr, size_t len, int prot, int flags, int fildes, off_t off), { return (void*) 0; })
+MAESTRO_STUB(int, munmap(void *addr, size_t len), { return -1; })
+
+/* stubs for functions required by libc/stdlib */
+MAESTRO_STUB(void, __assert_func(const char *file, int line, const char *failedexpr), { })
+
+MAESTRO_STUB(void, _start(void), { })
---
+++ newlib/libc/sys/hipperos/dummysys.c
@@ -0,0 +1,1 @@
+void not_required_by_hipperos(void) {}
